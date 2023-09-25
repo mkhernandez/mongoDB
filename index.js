168db8515f9cbac16e5a8202de56ebf037b8d1e2
Mark Hernandez <markkimhernandez@gmail.com>
2023-09-24 20:56:19 -0500

    Added multiple movies to our example movieApp db under the movies collection

--- index.js
+++ index.js
@@ -24,3 +24,24 @@ const movieSchema = new mongoose.Schema({
 // database movieApp
 const Movie = mongoose.model('Movie', movieSchema);
 
+// Can add a movie this way but would need to use blade.save() after this statement.
+// const blade = new Movie({title: "Blade", year: 1998, score: 7.1, rating: "R"});
+
+// Not the way this should be done but we just want some 
+// seed data for this movie example
+Movie.insertMany([
+    {title: "Amadeus", year: 1986, score: 9.2, rating: "R"},
+    {title: "Star Wars", year: 1977, score: 9.0, rating: "PG"},
+    {title: "Alien", year: 1979, score: 8.1, rating: "R"},
+    {title: "The Iron Giant", year: 1999, score: 7.5, rating: "PG"},
+    {title: "Moonrise Kingdom", year: 2012, score: 7.8, rating: "PG-13"}
+])
+.then(data => {
+    log("IT WORKED!");
+    log(data);
+})
+.catch(err => {
+    log("WE FUCKED SOMEWHERE UP!");
+    log(err);
+});
+
