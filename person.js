1a564cbdbcd240c032b8674cf788d1e80588ce48
Mark Hernandez <markkimhernandez@gmail.com>
2023-09-26 15:52:20 -0500

    Created a simple mongoose middelware example

--- person.js
+++ person.js
@@ -14,8 +14,22 @@ const personSchema = new mongoose.Schema({
     last: String
 })
 
+// This is a mongoose virtual example
 personSchema.virtual('fullName').get(function() {
     return `${this.first} ${this.last}`
+});
+
+// The following are examples of middleware in mongoose
+// The first example will happen right before we save to the people collection
+// Keep in mind we have access to 'this'
+personSchema.pre("save", async function() {
+    this.first = "YO";
+    this.last = "MAMA";
+    log("ABOUT TO SAVE!");
+});
+// This will come after the save has happened
+personSchema.post("save", async function() {
+    log("JUST SAVED!");
 });
 
 const Person = mongoose.model("Person", personSchema);
